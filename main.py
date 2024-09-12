7545c0ac085133212ee97274acbde877d14bcdcf
Rafael Freesz <rafaelfreesz@gmail.com>
2024-09-12 16:00:12 -0300

    Grafico testado funcionando

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import fuzzylib as fl
-import fuzzygroup as fg
 import plotly.graph_objects as go
 import numpy as np
 
@@ -11,14 +10,18 @@ groups = fl.prepare("01_wash/wash.csv")
 st.write("GRAFICOS")
 # st.line_chart(groups[0].function(1))
 
-def uuu(x):
-    return x**3
+def apply(xs,fun):
+    res = []
+    for x in range(len(xs)):
+        res.append(fun(x))
+    return res
 
-xi = np.linspace(0,10,11)
+xi = np.linspace(0,100,200)
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=xi, y=uuu(xi), mode='lines', name='lines'))
-fig.add_trace(go.Scatter(x=xi, y=200*xi+2, mode='lines', name='bull'))
+fig.add_trace(go.Scatter(x=xi, y=apply(xi,groups[0].function), mode='lines', name=f"{groups[0].f_name}_{groups[0].f_spec}"))
+fig.add_trace(go.Scatter(x=xi, y=apply(xi,groups[1].function), mode='lines', name=f"{groups[1].f_name}_{groups[1].f_spec}"))
+fig.add_trace(go.Scatter(x=xi, y=apply(xi,groups[2].function), mode='lines', name=f"{groups[2].f_name}_{groups[2].f_spec}"))
 st.plotly_chart(fig)
 
 # k = fl.fuzzify(xs,function)
