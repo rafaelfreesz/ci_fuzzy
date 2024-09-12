7293f128f4925bdec714e06e4ffe19bca9ab8b61
Rafael Freesz <rafaelfreesz@gmail.com>
2024-09-12 16:37:23 -0300

    Colocandoa lgumas funções no arquivo utils.py

--- main.py
+++ main.py
@@ -2,27 +2,26 @@ import streamlit as st
 import fuzzylib as fl
 import plotly.graph_objects as go
 import numpy as np
+import utils as ut
 
 
+groups = fl.prepare("01_wash/wash_x.csv")
 
-groups = fl.prepare("01_wash/wash.csv")
+k = ut.group_by(groups,"f_name")
 
-st.write("GRAFICOS")
-# st.line_chart(groups[0].function(1))
 
-def apply(xs,fun):
-    res = []
-    for x in range(len(xs)):
-        res.append(fun(x))
-    return res
+print(k)
 
-xi = np.linspace(0,100,200)
+# xi = np.linspace(0,100,200)
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=xi, y=apply(xi,groups[0].function), mode='lines', name=f"{groups[0].f_name}_{groups[0].f_spec}"))
-fig.add_trace(go.Scatter(x=xi, y=apply(xi,groups[1].function), mode='lines', name=f"{groups[1].f_name}_{groups[1].f_spec}"))
-fig.add_trace(go.Scatter(x=xi, y=apply(xi,groups[2].function), mode='lines', name=f"{groups[2].f_name}_{groups[2].f_spec}"))
-st.plotly_chart(fig)
+# fig = go.Figure()
+# fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[0].function), mode='lines', name=f"{groups[0].f_name}_{groups[0].f_spec}"))
+# fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[1].function), mode='lines', name=f"{groups[1].f_name}_{groups[1].f_spec}"))
+# fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[2].function), mode='lines', name=f"{groups[2].f_name}_{groups[2].f_spec}"))
+
+# st.write("GRAFICOS")
+# st.plotly_chart(fig)
+# st.write("FIM")
 
 # k = fl.fuzzify(xs,function)
 # print(k)
