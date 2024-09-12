f9f39d8d72f1355d0277eb9c6caa7ffbc168e272
Rafael Freesz <rafaelfreesz@gmail.com>
2024-09-12 13:23:52 -0300

    Incluindo funções para impressao e demais atributos no objeto FUZZY

--- fuzzygroup.py
+++ fuzzygroup.py
@@ -1,12 +1,31 @@
+import fuzzyfunctions as ff
 class Fuzzy:
     def __init__(self) -> None:
-        self.t_function="none"
+        self.f_type="none"
+        self.f_name="none"
+        self.f_spec="none"
         self.a=""
         self.b=""
         self.c=""
         self.d=""
         self.m=""
-        self.function=""
+        self.function= None
     
     def __str__(self) -> str:
-        return f"t_function: {self.t_function}  \na: {self.a}  \nb: {self.b}  \nc: {self.c}  \nd: {self.d}  \nm: {self.m}"
+        return f"f_name: {self.f_name}  \nf_spec: {self.f_spec}  \nf_type: {self.f_type}  \na: {self.a}  \nb: {self.b}  \nc: {self.c}  \nd: {self.d}  \nm: {self.m}"
+    
+    def define_function(self):
+        if self.f_type == 'trap_asc':
+            def function(x):
+                return ff.trap_asc(self.a,self.b,x)
+        elif self.f_type == 'trap_desc':
+            def function(x):
+                return ff.trap_desc(self.c,self.d,x)
+        elif self.f_type == 'trap_full':
+            def function(x):
+                return ff.trap_full(self.a,self.b,self.c,self.d,x)
+        else:
+            print("DUMB")
+
+
+    
