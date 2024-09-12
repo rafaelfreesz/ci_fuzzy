07319e533274d87627bdec0b01bfbbff1f4a16db
Rafael Freesz <rafaelfreesz@gmail.com>
2024-09-12 13:25:17 -0300

    Criando objetos do tipo função, com base no objeto de entrada

--- fuzzylib.py
+++ fuzzylib.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import fuzzyfunctions as ff
+import fuzzygroup as fg
+
 def fuzzify(xs, function):
     ys=[]
     for x in xs:
@@ -7,9 +10,28 @@ def fuzzify(xs, function):
 
 def prepare_run(filename):
     file = pd.read_csv(filename)
-    print(file)
-    # file.set_index('f_name') talvez nao precise
-    # build_functions()
+    build_functions(file)
 
-# def build_functions():
+def build_functions(file):
+    fuzzy_groups=[]
+    for a in range(len(file)):
+        obj=fg.Fuzzy()
+        obj.f_type = file.iloc[a]['f_type']
+        obj.f_name = file.iloc[a]['f_name']
+        obj.f_spec = file.iloc[a]['f_spec']
+        obj.a = file.iloc[a]['a']
+        obj.b = file.iloc[a]['b']
+        obj.c = file.iloc[a]['c']
+        obj.d = file.iloc[a]['d']
+        obj.m = file.iloc[a]['m']
+        obj.define_function()
+        fuzzy_groups.append(obj)
+     
+    print(len(fuzzy_groups))
+    for a in range(len(file)):
+        print(fuzzy_groups[a])
+
+    # print(type(file))
+
+
     
