9487e7f0e0bd955eb545a0467476ebd661e15104
Rafael Freesz <rafaelfreesz@gmail.com>
2024-09-12 21:18:25 -0300

    colocando e testando uma reta vertical

--- main.py
+++ main.py
@@ -12,20 +12,21 @@ k = ut.group_by(groups,"f_name")
 
 print(k)
 
-# xi = np.linspace(0,100,200)
+xi = np.linspace(0,100,200)
 
-# fig = go.Figure()
-# fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[0].function), mode='lines', name=f"{groups[0].f_name}_{groups[0].f_spec}"))
-# fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[1].function), mode='lines', name=f"{groups[1].f_name}_{groups[1].f_spec}"))
-# fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[2].function), mode='lines', name=f"{groups[2].f_name}_{groups[2].f_spec}"))
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[0].function), mode='lines', name=f"{groups[0].f_name}_{groups[0].f_spec}"))
+fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[1].function), mode='lines', name=f"{groups[1].f_name}_{groups[1].f_spec}"))
+fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[2].function), mode='lines', name=f"{groups[2].f_name}_{groups[2].f_spec}"))
+fig.add_vline(x=21, line_width=3, line_dash="dash",line_color="green")
 
-# st.write("GRAFICOS")
-# st.plotly_chart(fig)
-# st.write("FIM")
+st.write("GRAFICOS")
+st.plotly_chart(fig)
+st.write("FIM")
 
 # k = fl.fuzzify(xs,function)
 # print(k)
 # for i in range(len(groups)):
 #     print(groups[i])
 
-# st.write("Hello World")
+st.write("Hello World")
