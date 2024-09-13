a789cb220ef29f457c4b4b83de3e3262d21e0287
Rafael Freesz <rafaelfreesz@gmail.com>
2024-09-13 00:17:35 -0300

    Incluindo texto com resultados da fuzzificação.

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ if problema_selecionado == "Máquina de Lavar":
     
     sujeira = st.sidebar.slider("Selecione a quantidade de sujeira:", min_value=0, max_value=100,value=50,step=1)
 
-    graph=pmaq.set_graph(sujeira)
+    graph_sujeira,str_resultado_sujeira=pmaq.set_values(sujeira)
 
-    st.write("GRAFICOS")
-    st.plotly_chart(graph)
+    st.write("## 1. Fuzzificação")
+    st.plotly_chart(graph_sujeira)
+    st.warning(str_resultado_sujeira)
     st.write("FIM")
--- probmaq.py
+++ probmaq.py
@@ -3,21 +3,23 @@ import fuzzylib as fl
 import utils as ut
 import plotly.graph_objects as go
 
-def set_graph(sujeira):
+def set_values(sujeira):
     groups = fl.prepare("01_wash/wash_x.csv")
     k = ut.group_by(groups,"f_name")
 
-    fig = go.Figure()
+    graph_sujeira = go.Figure()
     x_ps = np.linspace(0,int(groups[0].b),int(groups[0].b)+1)
     x_ms = np.linspace(int(groups[1].a),int(groups[1].b),int(groups[1].b)+1)
     x_gs = np.linspace(int(groups[2].a),int(groups[2].m),int(groups[2].m-groups[2].a)+1)
 
-    fig.add_trace(go.Scatter(x=x_ps, y=ut.array_apply(x_ps,groups[0].f), mode='lines', name=f"{groups[0].f_name}_{groups[0].f_spec}"))
-    fig.add_trace(go.Scatter(x=x_ms, y=ut.array_apply(x_ms,groups[1].f), mode='lines', name=f"{groups[1].f_name}_{groups[1].f_spec}"))
-    fig.add_trace(go.Scatter(x=x_gs, y=ut.array_apply(x_gs,groups[2].f), mode='lines', name=f"{groups[2].f_name}_{groups[2].f_spec}"))
-    fig.add_vline(x=sujeira, line_width=3, line_dash="dash",line_color="green")
+    graph_sujeira.add_trace(go.Scatter(x=x_ps, y=ut.array_apply(x_ps,groups[0].f), mode='lines', name=f"{groups[0].f_name}_{groups[0].f_spec}"))
+    graph_sujeira.add_trace(go.Scatter(x=x_ms, y=ut.array_apply(x_ms,groups[1].f), mode='lines', name=f"{groups[1].f_name}_{groups[1].f_spec}"))
+    graph_sujeira.add_trace(go.Scatter(x=x_gs, y=ut.array_apply(x_gs,groups[2].f), mode='lines', name=f"{groups[2].f_name}_{groups[2].f_spec}"))
+    graph_sujeira.add_vline(x=sujeira, line_width=3, line_dash="dash",line_color="green")
 
-    fig.update_layout(width=480, height = 180, margin = dict(t=20,b=0), title = "Quantidade de Sujeira")
-    return fig
+    graph_sujeira.update_layout(width=480, height = 180, margin = dict(t=20,b=0), title = "Quantidade de Sujeira")
+    str_resultado_sujeira = get_result_string(groups,sujeira)
+    return (graph_sujeira,str_resultado_sujeira)
 
-# def printN
+def get_result_string(groups,sujeira):
+    return f"Sujeira: x = {sujeira}  \n Pouca Sujeira: {groups[0].f(sujeira)}  \n Média Sujeira: {groups[1].f(sujeira)}  \n Grande Sujeira: {groups[2].f(sujeira)}"
