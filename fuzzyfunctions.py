1daa439a8d8e2c21e208a7431ec517fdfa3a4ebf
Rafael Freesz <rafaelfreesz@gmail.com>
2024-09-12 22:21:03 -0300

    Implementando as funções do Triângulo

--- fuzzyfunctions.py
+++ fuzzyfunctions.py
@@ -1,3 +1,4 @@
+#Trapeze Functions
 def trap_desc(c,d,x):
     if x <= c:
         return 1
@@ -5,6 +6,7 @@ def trap_desc(c,d,x):
         return 0
     else:
         return (d-x)/(d-c)
+    
 def trap_asc(a,b,x):
     if x <= a:
         return 0
@@ -12,6 +14,7 @@ def trap_asc(a,b,x):
         return 1
     else:
         return (x-a)/(b-a)
+    
 def trap_full(a,b,c,d,x):
     if x <= a or x >= d:
         return 0
@@ -21,3 +24,24 @@ def trap_full(a,b,c,d,x):
         return (d-x)/(d-c)
     else:
         return 1
+
+#Triangle Functions
+def tri_desc(m,b,x):
+    if x < m or x > b:
+        return 0
+    else:
+        return (b-x)/(b-m)
+    
+def tri_asc(a,m,x):
+    if x < a or x > m:
+        return 0
+    else:
+        return (x-a)/(m-a)
+    
+def tri_full(a,b,m,x):
+    if x >= a and x <= m:
+        return (x-a)/(m-a)
+    elif x > m and x < b:
+        return (b-x)/(b-m)
+    else:
+        return 0
