45e66625fcd892fbf6ebcb7353a18d557cbc44dd
Rafael Freesz <rafaelfreesz@gmail.com>
2024-09-12 21:26:05 -0300

    Acrescentando slides e salvando dado obtido

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ k = ut.group_by(groups,"f_name")
 
 print(k)
 
-xi = np.linspace(0,100,200)
+xi = np.linspace(0,100,101)
+
+x_selecionado = st.sidebar.slider("Selecione a idade:", min_value=0, max_value=100,value=50,step=1)
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[0].function), mode='lines', name=f"{groups[0].f_name}_{groups[0].f_spec}"))
 fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[1].function), mode='lines', name=f"{groups[1].f_name}_{groups[1].f_spec}"))
 fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[2].function), mode='lines', name=f"{groups[2].f_name}_{groups[2].f_spec}"))
-fig.add_vline(x=21, line_width=3, line_dash="dash",line_color="green")
+fig.add_vline(x=x_selecionado, line_width=3, line_dash="dash",line_color="green")
+
 
 st.write("GRAFICOS")
 st.plotly_chart(fig)
