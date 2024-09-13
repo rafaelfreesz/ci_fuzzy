abfc2f1540f962da07e274bc1e02ebc006f06609
Rafael Freesz <rafaelfreesz@gmail.com>
2024-09-12 21:37:18 -0300

    Colocando um selectbox para selecionar HOME ou o problema

--- main.py
+++ main.py
@@ -14,22 +14,29 @@ print(k)
 
 xi = np.linspace(0,100,101)
 
-x_selecionado = st.sidebar.slider("Selecione a idade:", min_value=0, max_value=100,value=50,step=1)
+problema_selecionado = st.sidebar.selectbox(
+    "Selecione um Problema:",
+    ("Home","Máquina de Lavar","Autopeças"),
+    index=1
+)
+if problema_selecionado == "Máquina de Lavar":
+    print(problema_selecionado)
+    x_selecionado = st.sidebar.slider("Selecione a idade:", min_value=0, max_value=100,value=50,step=1)
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[0].function), mode='lines', name=f"{groups[0].f_name}_{groups[0].f_spec}"))
-fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[1].function), mode='lines', name=f"{groups[1].f_name}_{groups[1].f_spec}"))
-fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[2].function), mode='lines', name=f"{groups[2].f_name}_{groups[2].f_spec}"))
-fig.add_vline(x=x_selecionado, line_width=3, line_dash="dash",line_color="green")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[0].function), mode='lines', name=f"{groups[0].f_name}_{groups[0].f_spec}"))
+    fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[1].function), mode='lines', name=f"{groups[1].f_name}_{groups[1].f_spec}"))
+    fig.add_trace(go.Scatter(x=xi, y=ut.array_apply(xi,groups[2].function), mode='lines', name=f"{groups[2].f_name}_{groups[2].f_spec}"))
+    fig.add_vline(x=x_selecionado, line_width=3, line_dash="dash",line_color="green")
 
 
-st.write("GRAFICOS")
-st.plotly_chart(fig)
-st.write("FIM")
+    st.write("GRAFICOS")
+    st.plotly_chart(fig)
+    st.write("FIM")
 
-# k = fl.fuzzify(xs,function)
-# print(k)
-# for i in range(len(groups)):
-#     print(groups[i])
+    # k = fl.fuzzify(xs,function)
+    # print(k)
+    # for i in range(len(groups)):
+    #     print(groups[i])
 
-st.write("Hello World")
+    st.write("Hello World")
